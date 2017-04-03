c64fc0ec19b559365f94c7cc3da34d9f90b58031
David Byrne <phoenixdb8@gmail.com>
2017-04-03 19:02:09 +0100

    changed around the lights method, testing

--- SmartHomeServant.java
+++ SmartHomeServant.java
@@ -1,34 +1,37 @@
 import SmartHome.*;
 import java.util.*;
 
-class SmartHomeServant extends _HouseImplBase
-{
+class SmartHomeServant extends _HouseImplBase{
   final Scanner scanner = new Scanner(System.in);
+  String statusString = "";
+  Boolean status == false;
 
-
-    public Boolean lights(Boolean switch)
-    {
-      Boolean status = switch;
+    public String lights(Boolean lightSwitch){
+      Boolean status = lightswitch;
       if(status == false){
-        System.out.println("Lights are off, do you want turn on the lights");
+        System.out.println("Lights are off, do you want turn them on? Y/N");
         String lightChange = scanner.nextLine();
-        if(lightChange.equals("yes")){
-          System.out.println("Lights are now on");
-          status = true;
-          return true;
+        if(lightChange.equals("Y")){
+          statusString = "Lights are on";
+          status == true;
         }
-      }else if(status == true){
+        else{
+          System.out.println("Lights remain off");
+        }
+      }
+      else if(status == true){
         System.out.println("Lights are on, do you want turn off the lights");
         String lightChange = scanner.nextLine();
-        if(lightChange.equals("yes")){
+        if(lightChange.equals("Y")){
           System.out.println("Lights are now off");
-          status = false;
-          return false;
+          status == false;
+          String statusString = "Lights are off";
+        }
       }
-    } else{
-      System.out.println("Error");
+      else{
+        System.out.println("Error");
+      }
+
+      return statusString;
     }
-
-
-
-}
+  }
