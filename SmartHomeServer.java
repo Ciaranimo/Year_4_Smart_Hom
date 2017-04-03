922b799e7bb9c3fec1b6259e5fde8fe329fe348c
David Byrne <phoenixdb8@gmail.com>
2017-04-03 18:17:50 +0100

    structured contexts and objects of all 4 systems in Server

--- SmartHomeServer.java
+++ SmartHomeServer.java
@@ -33,76 +33,46 @@ public class SmartHomeServer{
 			//This is achieved by using the narrow function of the helper class
 			NamingContext rootCtx = NamingContextHelper.narrow(objRef);
 
-			// Add context 1 to root
-			nc[0] = new NameComponent("Context 1", "Context");
-			NamingContext Ctx1 = rootCtx.bind_new_context(nc);
-			System.out.println("Context 'Context 1' added to Name Space.");
+			// Add Lights context to root
+			nc[0] = new NameComponent("LightsCtx", "Context");
+			NamingContext lights = rootCtx.bind_new_context(nc);
+			System.out.println("Lights bound to root");
 
-			// Add context 2 to root
-			nc[0] = new NameComponent("Context 2", "Context");
-			NamingContext Ctx2 = rootCtx.bind_new_context(nc);
-			System.out.println("Context 'Context 2' added to Name Space.");
+			// Add Heat context to root
+			nc[0] = new NameComponent("HeatCtx", "Context");
+			NamingContext heat = rootCtx.bind_new_context(nc);
+			System.out.println("Heat bound to root");
 
-			// Add context 3 to root
-			nc[0] = new NameComponent("Context 3", "Context");
-			NamingContext Ctx3 = rootCtx.bind_new_context(nc);
-			System.out.println("Context 'Context 3' added to Name Space.");
+			// Add Door context to root
+			nc[0] = new NameComponent("DoorCtx", "Context");
+			NamingContext door = rootCtx.bind_new_context(nc);
+			System.out.println("Door bound to root");
 
+			// Add ALarm context to root
+			nc[0] = new NameComponent("AlarmCtx", "Context");
+			NamingContext alarm = rootCtx.bind_new_context(nc);
+			System.out.println("Alarm bound to root");
 
-			// Add Sub Context 2  to Context 1
-			nc[0] = new NameComponent("Sub-Context 2", "Context");
-			NamingContext SubCtx2 = Ctx1.bind_new_context(nc);
-			System.out.println("Context 'Sub-Context 2' added to Context 1.");
+			//Add LightsObject object to Lights context
+			nc[0] = new NameComponent("LightObject", "Object");
+			LightsCtx.rebind(nc, smartHomeRef);
+			System.out.println("Lights Object added to Lights context");
 
-			//Add Obj 1  to sub context 2
-			nc[0] = new NameComponent("Object 1", "Object");
-			//NameComponent path[] = {nc};
-			//Binding the name to an object that is stored in the Naming Context
-			SubCtx2.rebind(nc, SubCtx2);
-			System.out.println("Object 'Object 1' added to Sub Context 2.");
+			//Add HeatObject object to Heat context
+			nc[0] = new NameComponent("HeatObject", "Object");
+			HeatCtx.rebind(nc, smartHomeRef);
+			System.out.println("Heat Object added to Heat context");
 
-			//Add Obj 1  to Context 1
-			nc[0] = new NameComponent("Object 1", "Object");
-			//NameComponent path[] = {nc};
-			//Binding the name to an object that is stored in the Naming Context
-			Ctx1.rebind(nc, Ctx1);
-			System.out.println("Object 'Object 1' added to Context 1.");
+			//Add DoorObject object to Doors context
+			nc[0] = new NameComponent("DoorObject", "Object");
+			DoorCtx.rebind(nc, smartHomeRef);
+			System.out.println("Door Object added to Door context");
 
-			//Add Obj 2  to context 2
-			nc[0] = new NameComponent("Object 2", "Object");
-			//NameComponent path[] = {nc};
-			//Binding the name to an object that is stored in the Naming Context
-			Ctx2.rebind(nc, Ctx2);
-			System.out.println("Object 'Object 2' added to Context 2.");
+			//Add AlarmObject object to Alarm context
+			nc[0] = new NameComponent("AlarmObject", "Object");
+			AlarmCtx.rebind(nc, smartHomeRef);
+			System.out.println("Alarm Object added to Alarm context");
 
-			//Add context sub context 2 to Context 1
-			nc[0] = new NameComponent("Sub Context 1", "Context");
-			NamingContext SubCtx1 = Ctx2.bind_new_context(nc);
-			System.out.println("Context 'Sub Conetxt 1' added to Context 2.");
-
-			//Add Obj 3  to sub context 1
-			nc[0] = new NameComponent("Object 3", "Object");
-			//NameComponent path[] = {nc};
-			//Binding the name to an object that is stored in the Naming Context
-			SubCtx1.rebind(nc, SubCtx1);
-			System.out.println("Object 'Object 3' added to Sub Context 1.");
-
-			//Add Obj 4  to Context 2
-			// *** Method Bound to Object 4 *******
-			nc[0] = new NameComponent("Object 4", "Object");
-			//NameComponent path[] = {nc};
-			//Binding the name to an object that is stored in the Naming Context
-			Ctx2.rebind(nc, Ctx2);
-		// ADD METHOD BIND
-			Ctx2.rebind(nc, smartHomeRef);
-			System.out.println("Object 'Object 4' added to Context 2.");
-
-			//Add Obj 5  to Context 3
-			nc[0] = new NameComponent("Object 5", "Object");
-			//NameComponent path[] = {nc};
-			//Binding the name to an object that is stored in the Naming Context
-			Ctx3.rebind(nc, Ctx3);
-			System.out.println("Object 'Object 5' added to Context 3.");
 
     // stand by for method call from client
 			orb.run();
