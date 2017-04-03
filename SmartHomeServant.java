e38b3c4a945c52a047f57f1bb12e731511369f9f
Ciaran <ciaran426@yahoo.co.uk>
2017-04-03 17:01:48 +0100

    updated idl and java files to include additional methods

--- SmartHomeServant.java
+++ SmartHomeServant.java
@@ -8,6 +8,45 @@ class SmartHomeServant extends _HouseImplBase
     {
       String result = ("lights on");
       return result;
-      //System.out.println("TEST");
     }
+    public String turnOffLights()
+    {
+      String result = ("lights off");
+      return result;
+    }
+
+    public String turnOnHeating()
+    {
+      String result = ("heating on");
+      return result;
+    }
+    public String turnOffHeating()
+    {
+      String result = ("heating off");
+      return result;
+    }
+
+    public String lockDoors()
+    {
+      String result = ("doors locked");
+      return result;
+    }
+    public String unlockDoors()
+    {
+      String result = ("doors unlocked");
+      return result;
+    }
+
+    public String armAlarm()
+    {
+      String result = ("Alarm armed");
+      return result;
+    }
+
+    public String disarmAlarm()
+    {
+      String result = ("Alarm diasarmed");
+      return result;
+    }
+
 }
